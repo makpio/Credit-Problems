182bb3a760e33a2ec0e83a8892c47257efe97d6d
Mateusz <matiziel10@gmail.com>
2020-01-24 22:05:53 +0100

    Add tree classifier

--- creditproblems.py
+++ creditproblems.py
@@ -1,11 +1,17 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
-
+from sklearn import tree
 
 data = pd.read_csv("data.csv")
 
-print(data.head())
+y = data['Y']
+x = data.drop('Y', axis = 1)
 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.33, random_state = 0)
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.33, random_state = 0)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(x_train, y_train)
+
+print(clf.predict(x_test))
+
 
