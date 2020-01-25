c975c85b859a60e697fccb46d7f1549c8f7c5350
Mateusz <matiziel10@gmail.com>
2020-01-25 20:50:07 +0100

    Added logistic regression

--- creditproblems.py
+++ creditproblems.py
@@ -1,6 +1,8 @@
 import pandas as pd 
-from sklearn.model_selection import train_test_split, GridSearchCV
+from scipy.stats import uniform
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 from sklearn import tree, linear_model
+from sklearn.linear_model import LogisticRegression 
 
 
 def compare(arg1, arg2):
@@ -39,37 +41,54 @@ x = data.drop('Y', axis = 1)
 
 
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
 
 
-params = {
-    'penalty':['l1', 'l2', 'elasticnet'],        # l1 is Lasso, l2 is Ridge
-    'solver':['liblinear']
-    # 'C': np.linspace(0.00002,1,100)
-}
-
-lr = linear_model.LogisticRegression()
-lr_gs = GridSearchCV(lr, params).fit(x_train, y_train)
-
-print (compare(list(lr_gs.predict(x_test)), list(y_test)))
-# parameters = {
-#     'penalty' : ('l1', 'l2', 'elasticnet', 'none'),
-#     'dual' : (True, False), 
-#     'C' : [0.5, 2.0], 
-#     'fit_intercept' : (True, False)
+# params = {
+#     'penalty':['l1', 'l2', 'elasticnet'],        # l1 is Lasso, l2 is Ridge
+#     'solver':['liblinear']
+#     # 'C': np.linspace(0.00002,1,100)
 # }
 
+# lr = linear_model.LogisticRegression()
+# lr_gs = GridSearchCV(lr, params).fit(x_train, y_train)
+
+# print (compare(list(lr_gs.predict(x_test)), list(y_test)))
 
 
-# lr = linear_model.LogisticRegression(random_state=0, max_iter=30000)
-
-# clf = GridSearchCV(lr, parameters)
-# clf.fit(x_train, y_train)
-
-# print (compare(list(clf.predict(x_test)), list(y_test)))
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 0)
 
 
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.7, random_state = 0)
+parameters = {
+    'penalty' : ('l1', 'l2'),
+    'C' : [0.5, 2.0], 
+    'fit_intercept' : (True, False),
+    'intercept_scaling' : [0.0, 2.0],
+    'class_weight' : ('balanced', None),
+    'random_state' : [0, 10],
+    'multi_class' : ('auto', 'ovr', 'multinomial')
+}
+
+
+lr = linear_model.LogisticRegression(solver='saga', tol=1e-2)
+
+clf = GridSearchCV(lr, parameters)
+clf.fit(x_train, y_train)
+
+print (compare(list(clf.predict(x_test)), list(y_test)))
+
+
 # clf = linear_model.LogisticRegression(random_state=0, max_iter=30000).fit(x_train, y_train)
 
-# print(compare(list(clf.predict(x_test)), list(y_test)))
+
+#********************************************************************************************
+#********************************************************************************************
+
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state = 0)
+
+# logistic = LogisticRegression(solver='saga', tol=1e-2, max_iter=200, random_state=0)
+# distributions = dict(C = uniform(loc=0, scale=4), penalty = ['l2', 'l1'])
+# clf = RandomizedSearchCV(logistic, distributions, random_state=0)
+# search = clf.fit( x_train, y_train)
+
+# print (compare(list(search.predict(x_test)), list(y_test)))
