6bd0a00f6a7951e25d304536c7aadf39164d49e7
Mateusz <matiziel10@gmail.com>
2020-01-25 23:30:00 +0100

    Add test results

--- main.py
+++ main.py
@@ -35,21 +35,21 @@ for testSize in [0.1, 0.3, 0.5, 0.7, 0.9]:
             )
     )
 
-    lr_X.append(100 * testSize)
-    lr_Y.append(
-            compare(
-                logisticRegression(x_train = x_train, y_train = y_train, x_test = x_test),
-                list(y_test)
-            )
-    )
-    print(i)
+    # lr_X.append(100 * testSize)
+    # lr_Y.append(
+    #         compare(
+    #             logisticRegression(x_train = x_train, y_train = y_train, x_test = x_test),
+    #             list(y_test)
+    #         )
+    # )
+    print(testSize)
 
 with open("results/decisionTree.txt", "w") as file:
     for i in range(len(dt_X)):
         file.write( str(dt_X[i]) + '\n')
         file.write( str(dt_Y[i]) + '\n')
 
-with open("results/logisticRegression.txt", "w") as file:
-    for i in range(len(lr_X)):
-        file.write( str(lr_X[i]) + '\n')
-        file.write( str(lr_Y[i]) + '\n')
+# with open("results/logisticRegression.txt", "w") as file:
+#     for i in range(len(lr_X)):
+#         file.write( str(lr_X[i]) + '\n')
+#         file.write( str(lr_Y[i]) + '\n')
