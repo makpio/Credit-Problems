696a1965d20615fe444cb2b725befac38c0fe8f3
Mateusz <matiziel10@gmail.com>
2020-01-26 00:40:26 +0100

    final tests v3

--- graphs.py
+++ graphs.py
@@ -32,12 +32,12 @@ with open("results/logisticRegression.txt", "r") as file:
         lr_y.append(float(s2) * 100)
 
 plt.plot(dt_x, dt_y, label = 'Decision tree')
-plt.plot(lr_x, lr_y, label = 'Logistic regression')
+# plt.plot(lr_x, lr_y, label = 'Logistic regression')
 
 plt.ylabel('Procent dobrych klasyfikacji')
 plt.xlabel('Procent danych testowych')
 
-plt.title('Porownanie')
+plt.title('Decision tree')
 plt.legend()
 
 plt.show()
